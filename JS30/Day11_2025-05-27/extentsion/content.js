5b3a29c7bef2581298f8a89ad4eaf87874ea5e88
Hagno1310 <hoangnh.ptit@gmail.com>
2025-05-27 15:17:57 +0700

    up file day 11 js30

---
+++ JS30/Day11_2025-05-27/extentsion/content.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('dt-text-black-mine')[0].innerText = 'Sinh viên UDU - Top 1 server Hoàng Quốc Việt';
+document.getElementsByClassName('dt-text-black-mine')[0].style.color = "blue"
+document.getElementsByClassName('dt-text-black-mine')[0].style.fontSize = "30px"
+document.getElementsByClassName('dt-text-black-mine')[0].style.textTransform = "uppercase"
+

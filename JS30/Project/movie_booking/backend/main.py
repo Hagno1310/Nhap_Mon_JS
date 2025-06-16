5824ab96959db5c5728e659bb84555f9df32f96b
Hagno1310 <hoangnh.ptit@gmail.com>
2025-06-16 20:49:55 +0700

    day 30 js30 tao db trong mysql, connect db bang fastAPI va dung JS de get mat khau. post account

--- JS30/Project/movie_booking/backend/main.py
+++ JS30/Project/movie_booking/backend/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Body
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import uvicorn
+import mysql.connector
 
 app = FastAPI()
+
+#Connect to MySQL database
+db = mysql.connector.connect(
+    host="localhost",
+    user="Hagno1310",
+    password="123456",
+    database="js"
+)
 
 # Mount static folders
 app.mount("/style", StaticFiles(directory="frontend/style"), name="style")
@@ -19,27 +27,48 @@ templates = Jinja2Templates(directory="frontend/templates")
 def read_home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
+@app.get("/home", response_class=HTMLResponse)
+def read_home(request: Request):
+    return templates.TemplateResponse("home2.html", {"request": request})
+
 # Example route cho trang login
 @app.get("/login", response_class=HTMLResponse)
 def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-# Example route cho trang register
 @app.get("/register", response_class=HTMLResponse)
 def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
-# ADMIN routes
+@app.get("/api/account", response_class=JSONResponse)
+def get_accounts():
+    try:
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM accounts")
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
+
+@app.post('/api/account', response_class=JSONResponse)
+def post_account(data: dict = Body(...)):
+    try:
+        cursor = db.cursor()
+        query = """ 
+            INSERT INTO accounts (email, password)
+            VALUES (%s, %s)
+        """
+
+        values = (
+            data["email"], data["password"]
+        )
+
+        cursor.execute(query, values)
+        db.commit()
+        return {"message": "Đã thêm thành công"}
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
+
 @app.get("/admin_movie", response_class=HTMLResponse)
-def admin_page(request: Request):
+def admin_movie_page(request: Request):
     return templates.TemplateResponse("admin_movie.html", {"request": request})
-
-# Example route cho trang home2
-@app.get("/home2", response_class=HTMLResponse) 
-def dashboard_page(request: Request):
-    return templates.TemplateResponse("home2.html", {"request": request})
-
-# Example route cho trang myticket
-@app.get("/myticket", response_class=HTMLResponse)
-def myticket(request: Request):
-    return templates.TemplateResponse("myticket.html", {"request": request})

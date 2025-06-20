7de3281b512c5bac25285dd6a13e3f7db32081ab
Hagno1310 <hoangnh.ptit@gmail.com>
2025-06-20 21:14:28 +0700

    Thực hành buổi sáng trên lớp

--- JS30/Project/movie_booking/backend/main.py
+++ JS30/Project/movie_booking/backend/main.py
@@ -40,6 +40,18 @@ def login_page(request: Request):
 def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
+@app.get("/admin_movie", response_class=HTMLResponse)
+def admin_movie_page(request: Request):
+    return templates.TemplateResponse("admin_movie.html", {"request": request})
+
+@app.get("/info", response_class=HTMLResponse)
+def info_page(request: Request):
+    return templates.TemplateResponse("info.html", {"request": request})
+
+@app.get("/detail", response_class=HTMLResponse)
+def detail_page(request: Request):
+    return templates.TemplateResponse("detail.html", {"request": request})
+
 @app.get("/api/account", response_class=JSONResponse)
 def get_accounts():
     try:
@@ -69,23 +81,22 @@ def post_account(data: dict = Body(...)):
     except Exception as e:
         return JSONResponse({"error": str(e)}, status_code=500)
 
-@app.get("/admin_movie", response_class=HTMLResponse)
-def admin_movie_page(request: Request):
-    return templates.TemplateResponse("admin_movie.html", {"request": request})
-
-@app.get("/info", response_class=HTMLResponse)
-def info_page(request: Request):
-    return templates.TemplateResponse("info.html", {"request": request})
-
-@app.get("/detail", response_class=HTMLResponse)
-def detail_page(request: Request):
-    return templates.TemplateResponse("detail.html", {"request": request})
 
 @app.get("/api/movies", response_class=JSONResponse)
 def get_movies(request: Request):
     try:
         cursor = db.cursor(dictionary=True)
         cursor.execute("SELECT * FROM movies")
+        data = cursor.fetchall()
+        return data
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
+
+@app.get("/api/theaters", response_class=JSONResponse)
+def get_theaters():
+    try:
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM theaters")
         data = cursor.fetchall()
         return data
     except Exception as e:
